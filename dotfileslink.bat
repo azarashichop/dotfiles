97d50516d00eb22fc7755294256d916f0f54280c
Yosuke Takahashi <yosuke.takahashi1978@gmail.com>
2023-02-17 10:23:21 +0900

    Junk file clean up.

--- dotfileslink.bat
+++ dotfileslink.bat
@@ -1,12 +1,8 @@
-rem dotfile sync program for win.
+:: dotfile sync program for win.
 
-mklink %HOME%\.bash_profile %HOME%\dotfiles\.bash_profile_win
-mklink %HOME%\.dircolors %HOME%\dotfiles\.dircolors.256dark
-mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrcwin
-mklink %HOME%\.gvimrc %HOME%\dotfiles\.gvimrc
+:: vimrc
 mklink %HOME%\_vimrc %HOME%\dotfiles\_vimrc
 mklink %HOME%\_gvimrc %HOME%\dotfiles\_gvimrc
-mklink %HOME%\.minttyrcwin_dark %HOME%\dotfiles\.minttyrcwin_dark
-mklink %HOME%\.minttyrcwin_light %HOME%\dotfiles\.minttyrcwin_light
-mklink %HOME%\.minttyrclin_dark %HOME%\dotfiles\.minttyrclin_dark
-mklink %HOME%\.minttyrclin_light %HOME%\dotfiles\.minttyrclin_light
+
+:: init.vim
+mklink %LOCALAPPDATA%\nvim\init.vim %HOME%\dotfiles\init.vim
